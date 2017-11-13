759c71434f00137a302ac3b76ece2e43224724d8
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-11-13 17:12:49 +0000

    restaurant controller setup and at point of save to db

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,15 @@
+class RestaurantsController < ApplicationController
+  def new; end
+
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  private
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :description)
+    end
+end
