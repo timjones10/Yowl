f6a23a1e78ec1801b11121cf5f4bf949c42889ba
Christine <splogy2001@hotmail.com>
2017-11-14 15:35:21 +0000

    Working feature test for new page view, not working feature test for adding a new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,15 +3,21 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+   @restaurant = Restaurant.find(params[:id])
+ end
+
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
-    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant = Restaurant.new(restaurant_params)
 
     @restaurant.save
     redirect_to @restaurant
   end
+
 
   private
     def restaurant_params
--- test/system/restaurants_test.rb
+++ test/system/restaurants_test.rb
@@ -5,4 +5,17 @@ class RestaurantsTest < ApplicationSystemTestCase
     visit restaurants_path
     assert_selector "h1", text: "Restaurants"
   end
+
+  # test "creating a restaurant" do
+  #   visit restaurants_path
+  #
+  #   click_on "New Restaurant"
+  #
+  #   fill_in "Name", with: "Creating a Restaurant"
+  #   fill_in "Description", with: "Tell me about your food!"
+  #
+  #   click_on "Save Restaurant"
+  #
+  #   assert_text "Creating a Restaurant"
+  # end
 end
