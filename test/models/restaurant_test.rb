398f2c076535cc11b04be12f137707c6cc4ad151
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-11-14 12:24:18 +0000

    first passing test with minitest

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not save restaurant without a name' do
+    restaurant = Restaurant.new
+    assert_not restaurant.save, 'Saved the restaurant without a name'
+  end
 end
