4ab4cf4e4b76a8c85a1e00594d4e6e531ec040bc
Canace Wong <canacewongtoday@hotmail.com>
2017-11-15 18:02:39 +0000

    added description as a required field

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -2,15 +2,15 @@ require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
 
-  test 'should not save restaurant without a name' do
-    restaurant = Restaurant.new
-    assert_not restaurant.save, 'Saved the restaurant without a name'
-  end
-
   test 'should not save restaurant without name 2' do
     restaurant = Restaurant.new
     restaurant.save
     assert_empty Restaurant.all, 'Saved the restaurant without a name'
+  end
+
+  test 'should not save restaurant without a name' do
+    restaurant = Restaurant.new
+    assert_not restaurant.save, 'Saved the restaurant without a name'
   end
 
   test 'should not save a restaurant without a description' do
