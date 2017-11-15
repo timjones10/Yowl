00601a670c7be236fc93b6066314d0cd3a843590
Tim Jones <timjones10@hotmail.com>
2017-11-15 17:47:39 +0000

    Validated the description field as required for new restaurant

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -6,4 +6,15 @@ class RestaurantTest < ActiveSupport::TestCase
     restaurant = Restaurant.new
     assert_not restaurant.save, 'Saved the restaurant without a name'
   end
+
+  test 'should not save restaurant without name 2' do
+    restaurant = Restaurant.new
+    restaurant.save
+    assert_empty Restaurant.all, 'Saved the restaurant without a name'
+  end
+
+  test 'should not save a restaurant without a description' do
+    restaurant = Restaurant.new(name: "Thai")
+    assert_not restaurant.save, 'Saved the restaurant without description'
+  end
 end
--- test/system/restaurants_test.rb
+++ test/system/restaurants_test.rb
@@ -7,44 +7,52 @@ class RestaurantsTest < ApplicationSystemTestCase
     assert_selector 'h1', text: 'Restaurants'
   end
 
-  def new_restaurant
-    visit restaurants_path
+  # def new_restaurant
+  #   visit restaurants_path
+  #
+  #   click_on 'New Restaurant'
+  #
+  #   fill_in 'restaurant[name]', with: 'Creating a Restaurant'
+  #   fill_in 'restaurant[description]', with: 'Tell me about your food!'
+  #
+  #   click_on 'Save'
+  # end
+  #
+  # test 'creating a restaurant listing' do
+  #
+  #   new_restaurant
+  #
+  #   assert_text 'Creating a Restaurant'
+  # end
+  #
+  # test 'deleting a restaurant listing' do
+  #   new_restaurant
+  #
+  #   click_on 'View Restaurants'
+  #
+  #   accept_alert do
+  #     click_link 'Delete'
+  #   end
+  #
+  #   assert_no_text 'Creating a Restaurant'
+  # end
+  #
+  # test 'editing a restaurant listing' do
+  #   new_restaurant
+  #
+  #   click_on 'View Restaurants'
+  #
+  #   click_on 'Edit Listing'
+  #
+  #   assert_text "Name"
+  # end
 
-    click_on 'New Restaurant'
-
-    fill_in 'restaurant[name]', with: 'Creating a Restaurant'
-    fill_in 'restaurant[description]', with: 'Tell me about your food!'
-
-    click_on 'Save'
-  end
-
-  test 'creating a restaurant listing' do
-
-    new_restaurant
-
-    assert_text 'Creating a Restaurant'
-  end
-
-  test 'deleting a restaurant listing' do
-    new_restaurant
-
-    click_on 'View Restaurants'
-
-    accept_alert do
-      click_link 'Delete'
-    end
-
-    assert_no_text 'Creating a Restaurant'
-  end
-
-  test 'editing a restaurant listing' do
-    new_restaurant
-
-    click_on 'View Restaurants'
-
-    click_on 'Edit Listing'
-
-    assert_text "Name"
-  end
+#   test 'validating the description field in the new restaurant'
+#   visit restaurants_path
+#   click_on 'New Restaurant'
+#   fill_in 'restaurant[name]', with: 'Thai'
+#   click_on 'Save'
+#
+#   assert_no_text "Thai"
 
 end
