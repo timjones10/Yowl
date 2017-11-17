ea6003b7a5c70de7c39aa4c54b0e0863e677df62
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-11-17 10:55:34 +0000

    able to delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,13 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.create(review_params)
-    # @rating = @restaurant.ratings.new :value => params[:value]
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.find(params[:id])
+    @review.destroy
     redirect_to restaurant_path(@restaurant)
   end
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
 end
